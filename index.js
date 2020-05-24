5630d046dd7b45e03654b105cb2b7367985541e6
Terry Cooper <coopertkm@gmail.com>
2020-05-24 22:54:03 +0100

    Added basic test suite

--- index.js
+++ index.js
@@ -11,7 +11,17 @@ function Phrase(content) {
 
   // Returns content processed for palindrome testing
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
+  }
+
+  // Returns the letters in the context
+  // e.g. "Hello, world!" returns "Helloworld"
+  this.letters = function letters() {
+    let theLetters = [];
+    for (let i = 0; i < this.content.length; i++) {
+      theLetters.push(this.content.charAt(i).match(/[a-zA-Z]/));
+    }
+    return theLetters.join("");
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
